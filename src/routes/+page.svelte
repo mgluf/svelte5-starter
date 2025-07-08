b72a96caad3b707a815a56facde53599eecd8f01
Matt <gluf.matt@gmail.com>
2025-07-08 19:58:34 -0400

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <h1>hello</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<br>
-<a href="/carousel">carousel</a>
 
 <style lang='scss'>
   @use "functions" as *;
